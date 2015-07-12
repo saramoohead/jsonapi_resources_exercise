cc36b8bd2de7c1d8fbf20919c797e9b778f7d834
Sara OC <saramoo@hotmail.com>
2015-07-12 21:31:48 +0100

    fail unit test for image_url

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -3,6 +3,11 @@ require 'shoulda/matchers'
 
 describe Image do
 
-    it { should have_one(:caption) }
+  it { should have_one(:caption) }
+
+  it 'is created' do
+    image = described_class.create(image_url: 'http://www.example.com/images/image.jpg')
+    expect(image.created_at).not_to be nil
+  end
 
 end
