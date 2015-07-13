07e6dd6e6262e77849a86afcf8e8a7ca48822934
Jamon Holmgren <jamon@clearsightstudio.com>
2015-07-12 17:31:11 -0700

    Fixed spec

--- spec/features/image_feature_spec.rb
+++ spec/features/image_feature_spec.rb
@@ -9,13 +9,12 @@ feature 'images' do
 
   scenario 'are created when route receives image url' do
     expect(Image.all.count).to eq 0
-    # request.env['HTTP_ACCEPT'] = 'application/vnd.api+json'
-    # request.env['CONTENT_TYPE'] = 'application/vnd.api+json'
-    # request.accept = "application/vnd.api+json"
-    # post :create, { "data": { "type": "images", "attributes": {"image_url": "http://www.example.com/images/image.jpg" } } }
-    # post "/images", { "data" => { "type" => "images", "image_url" => "http://www.example.com/images/image.jpg" } }
-    # post "/images", {format: :"json+api", "data" => { "type" => "images", "image_url" => "http://www.example.com/images/image.jpg" } }, {"CONTENT_TYPE" => "application/vnd.api+json"}
-    # expect(JSON.parse(last_response.body)).to eq({ not_sure: "what" })
+    post "/images", { "data" => { "type" => "images", "attributes" => { "image-url" => "http://www.example.com/images/image.jpg" } } }.to_json, { "CONTENT_TYPE" => "application/vnd.api+json" }
+    result = JSON.parse(last_response.body)["data"]
+    expect(result["id"].to_i).to be > 0
+    expect(result["type"]).to eq "images"
+    expect(result["links"]["self"]).to start_with "http://example.org/images/"
+    expect(result["attributes"]).to eq({ "image-url"=>"http://www.example.com/images/image.jpg" })
     expect(Image.all.count).to eq 1
   end
 
